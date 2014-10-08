47a9d5e66669be7b4a2b458f244bd3a9262b85d1
Laktou <trey.cook@ssrc.msstate.edu>
2014-10-08 15:32:16 -0500

    final form
    should be good to go, but need to test more extensively to ensure this

--- hw4/hw4/Stack.cpp
+++ hw4/hw4/Stack.cpp
@@ -1,40 +1,43 @@
 #include "Stack.h"
+#include <iostream>
 
 using namespace std;
 
-Stack::Stack(){					// Basic initializaiton that assigns default values to data members
+Stack::Stack(){							// Basic initializaiton that assigns default values to data members
 	top = -1;
 	capacity = 4;
 	stack = new int[capacity];
 }
 
-Stack::Stack(int size){				// Initialization that uses input to determine size of stack array
+Stack::Stack(int size){					// Initialization that uses input to determine size of stack array
 	top = -1;
 	capacity = size;
 	stack = new int[capacity];
 }
 
-Stack::~Stack(){					// Reset private data members and reallocate dynamic memory
+Stack::~Stack(){						// Reset private data members and reallocate dynamic memory
 	top = -1;
 	capacity = 0;
 	delete[] stack;
 }
 
-int Stack::pop(int& value){			// Returns value at the top of stack, removes it, and decrements top by one
-	if (!(Stack::is_empty())){		// If the stack is not empty
+int Stack::pop(int& value){				// Returns value at the top of stack, removes it, and decrements top by one
+	if (!(Stack::is_empty())){			// If the stack is not empty
 		if (size() <= (capacity / 2)){
 			resize(false);
 		}
+		value = stack[top];
 		--top;
 		return value;
 	}
 	else{
-		return 0;
+		cout << "Stack is empty!\n";
+		return false;
 	}
 	
 }
 
-void Stack::push(int value){				// Increments the top value and and places the given number at that location in the array
+void Stack::push(int value){			// Increments the top value and and places the given number at that location in the array
 	if ((capacity - top) <= 4){
 		resize(true);
 	}
@@ -42,11 +45,17 @@ void Stack::push(int value){				// Increments the top value and and places the g
 	stack[top] = value;
 }
 
-int Stack::peek(){					// Returns the value at the top of the stack but does not remove it
-	return stack[top];
+int Stack::peek(){						// Returns the value at the top of the stack but does not remove it
+	if (!(Stack::is_empty())){
+		return stack[top];
+	}
+	else {
+		cout << "Stack is empty!\n";
+		return false;
+	}
 }
 
-int Stack::size(){					// Returns the current size of the array
+int Stack::size(){						// Returns the current size of the array
 	return (top + 1);
 }
 
--- hw4/hw4/driver.cpp
+++ hw4/hw4/driver.cpp
@@ -1,11 +1,12 @@
 // Test program for Stack class
 #include "Stack.h"
 #include <iostream>
+
 using namespace std;
 
 int main(){
 	Stack testStack;
-	int popVal;
+	int popVal = 0;
 
 	cout << "Top of test stack is: " << testStack.peek() << endl;
 	testStack.push(64);
@@ -15,7 +16,8 @@ int main(){
 	testStack.push(32);
 	cout << "Top of test stack is now: " << testStack.peek() << endl;
 	testStack.pop(popVal);
-	cout << "Popped value " << popVal << endl;
+	cout << "Popped value: " << popVal << endl;
+	cout << "Top of test stack is now: " << testStack.peek() << endl;
 	
 	return 0;
 }
